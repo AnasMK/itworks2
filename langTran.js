e24b279423e399a7e97f0faa21abd7d2ed381223
vagrant <vagrant@homestead>
2018-08-01 23:36:49 +0000

    ayman enhancements

---
+++ langTran.js
@@ -0,0 +1,235 @@
+var dictionary = {
+    'title': {
+        'en': 'It Works - Home',
+        'ar': 'اخيرا اشتغلت - الصفحة الرئيسية'
+    },
+    'Homebtn': {
+        'en': 'Home',
+        'ar': 'الصفحة الرئيسية'
+    },
+    'Aboutbtn': {
+        'en': 'About',
+        'ar': 'تفاصيل عنا'
+    },
+    'Servicesbtn': {
+        'en': 'Service',
+        'ar': 'خدماتنا'
+    },
+    'Stroresbtn': {
+        'en': 'Strore',
+        'ar': 'المتجر'
+    },
+    'Contactbtn': {
+        'en': 'Contact',
+        'ar': 'تواصل معنا'
+    },
+    'Trans': {
+        'en': 'العربية',
+        'ar': 'English'
+    },
+    'bannerTitle': {
+        'en': 'Hajj Mubarak',
+        'ar': 'حج مبرور'
+    },
+    'bannerDis': {
+        'en': 'Ya Allah give us the chance to offer hajj. Ameen',
+        'ar': 'يا الله ارزقنا لنعطي الفرصة ليحج الجميع. امين'
+    },
+    'moreService': {
+        'en': 'See Services',
+        'ar': 'اطلع على خدماتنا'
+    },
+    'learnMore': {
+        'en': 'Learn More',
+        'ar': 'تعرف على تفاصيل اكثر'
+    },
+    'aboutHajj': {
+        'en': 'About Hajj',
+        'ar': 'تعرف على الحج'
+    },
+    'whHajj': {
+        'en': 'What is Hajj?',
+        'ar': 'ماهي شعيرت الحج؟'
+    },
+    'disHajj': {
+        'en': 'For those readers who don’t know about Hajj. Hajj (Pilgrimage) is an annual Islamic pilgrimage to Makkah,  and a mandatory religious duty for Muslims that must be carried out at least once in their lifetime by all adult Muslims who are physically and financially capable of undertaking the journey, and can support their family during their absence. It is one of the five pillars of Islam, alongside Shahadah, Salat, Zakat, and Sawm. The Hajj is the largest annual gatherings of people in the world. ',
+        'ar': 'الحج هو قصد الكعبة بيت الله الحرام لأداء المناسك وهي أفعال وأقوال جاءت عن النبي صلى الله عليه وسلم في صفة حجّه كالطواف سبعا بالبيت الحرام والسعي سبعا بين جبلي الصّفا والمروة والوقوف في عرفة ورمي الجمرات بمنى وغير ذلك ، وفيه منافع عظيمة للعباد من إعلان التوحيد لله والمغفرة العظيمة للحجاج والتعارف بين المسلمين وتعلّم أحكام الدّين وغير ذلك . واهتمامك بالسّؤال عن الحجّ أيّها الفتى مع صغر سنّك وبُعْد مكانك في كندا هو أمر جيد تُشكر عليه ، ونسأل الله لك التمكين من حجّ بيته وقضاء فرضه ، وصلى الله على نبينا محمد .'
+    },
+    'Freson': {
+        'en': 'It is undertaking one of the pillars of Islam without which it is not complete. ',
+        'ar': 'أنه قيام بأحد أركان الإسلام التي لا يتم إلا بها ، وهذا يدل على أهميته ومحبة الله له . ',
+    },
+    'Sreson': {
+        'en': ' It is a kind of jihad for the sake of Allah; hence Allah mentions it after the verses of jihad.',
+        'ar': 'أنه نوع من الجهاد في سبيل الله ، ولذلك ذكره الله تعالى بعد ذكر آيات الجهاد .',
+    },
+    'Treson': {
+        'en': ' here is an immense reward for the one who does it in the prescribed manner.',
+        'ar': 'الثواب الجزيل والأجر العظيم لمن قام به على الوجه المشروع ، فقد صح عن النبي صلى الله عليه وسلم أنه قال : (الحج المبرور ليس له جزاء إلا الجنة) وقال : (من حج فلم يرفث ولم يفسق خرج من ذنوبه كيوم ولدته أمه) ,',
+    },
+    'whService': {
+        'en': ' Our Services',
+        'ar': 'خدماتنا',
+    },
+    'FService': {
+        'en': ' Communicate with Hajj adviser',
+        'ar': 'تواصل مع مسؤول الحملة الخاص بك',
+    },
+    'FServiceDis': {
+        'en': ' will have to make sure you can ask any things and in any time, also we will guide you in Hajj.',
+        'ar': 'سنحرص على جميع استفساراتك سيتم الرد عليها على مدار الساعةو وسيقوم بتوجيهك لكل الخطوات اللي تحتاج مساعدة بها.',
+    },
+    'SService': {
+        'en': ' Request services easily',
+        'ar': 'اطلب احتياجاتك بسهولة',
+    },
+    'SServiceDis': {
+        'en': ' will have to make sure the all words is translating to your languge and whene you want any t',
+        'ar': 'سنحرص على ان جميع المصطلحات ستتم ترجمتها للغة بلدك لتكون سهلة بالفهم والتواصل وسيتم عن طريق تقنيات حديثة.',
+    },
+    'TService': {
+        'en': ' Locate you when you are lost',
+        'ar': 'نقوم ب ايجادك عن تعرضك للضياع',
+    },
+    'TServiceDis': {
+        'en': ' will have to make sure we handel the lost situation and make you safe.',
+        'ar': 'سنحرص على انقوم بالتعامل مع اي حالة ضياع ب اسرع توقيت لجعلك بعيد عن المخاطر.',
+    },
+    'FoService': {
+        'en': ' Full services for special need',
+        'ar': 'اطلب احتياجاتك بسهولة',
+    },
+    'FoServiceDis': {
+        'en': ' will have to make sure the help the the spical need people to finish all arkan of Hajj.',
+        'ar': 'سنقوم بالحرص على تلبية جميع مطالب ذوي الاحتياجات الخاصة وتوفير سبل الراحة لهم ليتتمو جميع مناسك الحج بسهولة ويسر.',
+    },
+    'FiService': {
+        'en': ' Display our statistics',
+        'ar': 'استعراض جميع احصائيات شركتنا',
+    },
+    'FiServiceDis': {
+        'en': ' will have to allow the user to cheack our statistics and that sorce of proude.',
+        'ar': 'نجعل للمستخدم حرية استعراض احصائياتنا بشكل كامل وذلك حرصا مننا على المصداقية ويعتبر مصدر فخر لنا.',
+    },
+    'SiService': {
+        'en': ' 24/7 Support',
+        'ar': 'خدماتنا ستكون على مدار الاربع وعشرين ساعى',
+    },
+    'SiServiceDis': {
+        'en': ' will have to make sure our service continou working for 24/7 hour to get full user satifiction.',
+        'ar': 'نحرص على ان خدماتنا ستستمر على مدى 24 ساعة حتى نحصل على رضى كامل من عملائنا.',
+    },
+    'Ffeture': {
+        'en': ' Handel lost situation',
+        'ar': 'التعامل مع حالات الضياع',
+    },
+    'Sfeture': {
+        'en': ' Hajj service rating',
+        'ar': 'تقييم خدماتنا',
+    },
+    'Tfeture': {
+        'en': ' User satisfaction',
+        'ar': 'رضا المشتركين بخدماتنا',
+    },
+    'Fofeture': {
+        'en': ' recommended service rating',
+        'ar': 'سبة اقتراح خدماتنا',
+    },
+    'faqQuistion': {
+        'en': ' Common Question',
+        'ar': 'رضا المشتركين بخدماتنا',
+    },
+    'FQuistion': {
+        'en': ' How can I apply to hajj',
+        'ar': 'كيف يمكنني التقدم للحج',
+    },
+    'FaQuistion': {
+        'en': ' All agencies handling Hajj visa applications are required to fill out and sign a contract issued by the Ministry of Hajj before submitting the passports to the Embassy for Hajj visas.  The contract details the rights and obligations of the agency and the pilgrim.  The agency is to keep a copy of the signed contract for their records in case the Embassy needs to refer to it at any time. ',
+        'ar': 'يطلب من جميع الوكالات التي تتعامل مع طلبات تأشيرات الحج أن تقوم بتعبئة وتوقيع عقد صادر عن وزارة الحج قبل تقديم جوازات السفر إلى السفارة للحصول على تأشيرات الحج. تفاصيل العقد حقوق والتزامات الوكالة والحاج. تحتفظ الوكالة بنسخة من العقد الموقّع لسجلاتها في حال احتاجت السفارة للإشارة إليها في أي وقت.ا',
+    },
+    'SQuistion': {
+        'en': ' What the benefit from your service',
+        'ar': 'يطلب من جميع الوكالات التي تتعامل مع طلبات تأشيرات الحج أن تقوم بتعبئة وتوقيع عقد صادر عن وزارة الحج قبل تقديم جوازات السفر إلى السفارة للحصول على تأشيرات الحج. تفاصيل العقد حقوق والتزامات الوكالة والحاج. تحتفظ الوكالة بنسخة من العقد الموقّع لسجلاتها في حال احتاجت السفارة للإشارة إليها في أي وقت.ا',
+    },
+    'SaQuistion': {
+        'en': ' Redug Lefes dolor sit amet, consectetur adipisicing elit. Aspernatur, tempore, commodi quas mollitia dolore magnam quidem repellat, culpa voluptates laboriosam maiores alias accusamus recusandae vero aperiam sint nulla beatae eos.',
+        'ar': 'يطلب من جميع الوكالات التي تتعامل مع طلبات تأشيرات الحج أن تقوم بتعبئة وتوقيع عقد صادر عن وزارة الحج قبل تقديم جوازات السفر إلى السفارة للحصول على تأشيرات الحج. تفاصيل العقد حقوق والتزامات الوكالة والحاج. تحتفظ الوكالة بنسخة من العقد الموقّع لسجلاتها في حال احتاجت السفارة للإشارة إليها في أي وقت.ا',
+    },
+    'TQuistion': {
+        'en': 'How much it cost',
+        'ar': 'متهي تكاليف خدماتكم بشكل كامل.',
+    },
+    'TaQuistion': {
+        'en': ' Redug Lefes dolor sit amet, consectetur adipisicing elit. Aspernatur, tempore, commodi quas mollitia dolore magnam quidem repellat, culpa voluptates laboriosam maiores alias accusamus recusandae vero aperiam sint nulla beatae eos. ',
+        'ar': 'يطلب من جميع الوكالات التي تتعامل مع طلبات تأشيرات الحج أن تقوم بتعبئة وتوقيع عقد صادر عن وزارة الحج قبل تقديم جوازات السفر إلى السفارة للحصول على تأشيرات الحج. تفاصيل العقد حقوق والتزامات الوكالة والحاج. تحتفظ الوكالة بنسخة من العقد الموقّع لسجلاتها في حال احتاجت السفارة للإشارة إليها في أي وقت.ا',
+    },
+    'FoQuistion': {
+        'en': ' Maiores alias accusamus ',
+        'ar': 'يطلب من جميع الوكالات التي تتعامل مع طلبات تأشيرات الحج ',
+    },
+    'FoaQuistion': {
+        'en': ' Redug Lefes dolor sit amet, consectetur adipisicing elit. Aspernatur, tempore, commodi quas mollitia dolore magnam quidem repellat, culpa voluptates laboriosam maiores alias accusamus recusandae vero aperiam sint nulla beatae eos.',
+        'ar': 'يطلب من جميع الوكالات التي تتعامل مع طلبات تأشيرات الحج أن تقوم بتعبئة وتوقيع عقد صادر عن وزارة الحج قبل تقديم جوازات السفر إلى السفارة للحصول على تأشيرات الحج. تفاصيل العقد حقوق والتزامات الوكالة والحاج. تحتفظ الوكالة بنسخة من العقد الموقّع لسجلاتها في حال احتاجت السفارة للإشارة إليها في أي وقت.ا',
+    },
+    'Fstep': {
+        'en': ' Apply',
+        'ar': 'التقدم على الحج ',
+    },
+    'Sstep': {
+        'en': ' Wait for approval ',
+        'ar': 'انتظار الموافقة ',
+    },
+    'Tastep': {
+        'en': ' Apply step ',
+        'ar': 'خطوة التقدم للحج',
+    },
+    'Tstep': {
+        'en': ' Come to Hajj ',
+        'ar': 'القديم للحج',
+    },
+    'TaSstep': {
+        'en': ' Redug Lares dolor sit amet, consectetur adipisicing elit. Animi vero excepturi magnam ducimus adipisci voluptas, praesentium maxime necessitatibus in dolor dolores unde ab, libero quo. Aut, laborum sequi. ',
+        'ar': 'ايطلب من جميع الوكالات التي تتعامل مع طلبات تأشيرات الحج أن تقوم بتعبئة وتوقيع عقد صادر عن وزارة الحج قبل تقديم جوازات السفر إلى السفارة للحصول على تأشيرات الحج. تفاصيل العقد حقوق والتزامات الوكالة والحاج.',
+    },
+    'TaSFstep': {
+        'en': ' voluptas, praesentium maxime cum fugiat,magnam ducimus adipisci voluptas, praesentium architecto ducimus, doloribus fuga itaque omnis placeat. ',
+        'ar': 'اايطلب من جميع الوكالات التي تتعامل مع طلبات تأشيرات الحج أن تقوم بتعبئة وتوقيع عقد صادر عن وزارة الحج قبل تقديم جوازات السفر إلى السفارة للحصول على تأشيرات الحج. تفاصيل العقد حقوق',
+    },
+    'Fatstep': {
+        'en': ' Visa approval step ',
+        'ar': 'خطوة قبول الفيزا',
+    },
+    'Fastsstep': {
+        'en': ' voluptas, praesentium maxime cum fugiat,magnam ducimus adipisci voluptas, praesentium architecto ducimus, doloribus fuga itaque omnis. ',
+        'ar': 'ايطلب من جميع الوكالات التي تتعامل مع طلبات تأشيرات الحج أن تقوم بتعبئة وتوقيع عقد صادر عن وزارة الحج قبل تقديم جوازات السفر إلى السفارة للحصول على تأشيرات الحج. تفاصيل العقد حقوقا',
+    },
+    'Faastsstep': {
+        'en': ' Redug Lares dolor sit amet, consectetur adipisicing elit. Animi vero excepturi magnam ducimus adipisci voluptas, praesentium maxime necessitatibus in dolor dolores unde ab, libero quo. Aut. ',
+        'ar': 'ايطلب من جميع الوكالات التي تتعامل مع طلبات تأشيرات الحج أن تقوم بتعبئة وتوقيع عقد صادر عن وزارة الحج قبل تقديم جوازات السفر إلى السفارة للحصول على تأشيرات الحج. تفاصيل العقد حقوقا',
+    },
+    'Siastsstep': {
+        'en': ' Coming to Hajj step ',
+        'ar': 'خطوة القدوم للحج',
+    },
+
+
+
+};
+var langs = ['en', 'ar'];
+var current_lang_index = 0;
+var current_lang = langs[current_lang_index];
+
+window.change_lang = function() {
+    current_lang_index = ++current_lang_index % 3;
+    current_lang = langs[current_lang_index];
+    translate();
+}
+
+function translate() {
+    $("[data-translate]").each(function(){
+        var key = $(this).data('translate');
+        $(this).html(dictionary[key][current_lang] || "N/A");
+    });
+}
+
+translate();
---
+++ store.php
@@ -0,0 +1,245 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+	<link rel="apple-touch-startup-image" href="assets/img/splash/launch-640x1136.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
+	<link rel="apple-touch-startup-image" href="assets/img/splash/launch-640x1136.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
+	<link rel="apple-touch-startup-image" href="assets/img/splash/launch-640x1136.png" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
+	<link rel="apple-touch-startup-image" href="assets/img/splash/launch-640x1136.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
+	<link rel="apple-touch-startup-image" href="assets/img/splash/launch-640x1136.png" media="(min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)">
+	<link rel="apple-touch-startup-image" href="assets/img/splash/launch-640x1136.png" media="(min-device-width: 834px) and (max-device-width: 834px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)">
+	<link rel="apple-touch-startup-image" href="assets/img/splash/launch-640x1136.png" media="(min-device-width: 1024px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)">  
+
+	<link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png">
+	<link rel="manifest" href="./manifest.json">
+	<link rel="mask-icon" href="./safari-pinned-tab.svg" color="#ffee00">
+
+
+	<meta charset="utf-8">
+	<title>ItWorks - Home</title>
+	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+	<meta content="" name="keywords">
+	<meta content="" name="description">
+
+	<!-- Favicons -->
+	<link href="assets/img/favicon.png" rel="icon">
+	<link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,700|Raleway:300,400,400i,500,500i,700,800,900" rel="stylesheet">
+
+	<!-- Bootstrap CSS File -->
+	<link href="assets/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- Libraries CSS Files -->
+	<link href="assets/lib/nivo-slider/css/nivo-slider.css" rel="stylesheet">
+	<link href="assets/lib/owlcarousel/owl.carousel.css" rel="stylesheet">
+	<link href="assets/lib/owlcarousel/owl.transitions.css" rel="stylesheet">
+	<link href="assets/lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+	<link href="assets/lib/animate/animate.min.css" rel="stylesheet">
+	<link href="assets/lib/venobox/venobox.css" rel="stylesheet">
+
+	<!-- Nivo Slider Theme -->
+	<link href="assets/css/nivo-slider-theme.css" rel="stylesheet">
+
+	<!-- Langauge felid style -->
+	<link href="assets/css/stylesLanguage.css?v2" rel="stylesheet">
+	
+
+	<!-- Main Stylesheet File -->
+	<link href="assets/css/style.css" rel="stylesheet">
+
+	<!-- Responsive Stylesheet File -->
+	<link href="assets/css/responsive.css" rel="stylesheet">
+
+  <!-- =======================================================
+    Theme Name: eBusiness
+    Theme URL: https://bootstrapmade.com/ebusiness-bootstrap-corporate-template/
+    Author: BootstrapMade.com
+    License: https://bootstrapmade.com/license/
+    ======================================================= -->
+</head>
+<body>
+
+	<!-- Start Store with Languge Area -->
+	<div class="langChoos">
+		<h3>Translate to</h3>
+		<select class="langOption">
+			<option value="volvo">Volvo</option>
+		</select>
+	</div>
+	<!-- Start Store with Languge Area -->
+
+	<!-- Start portfolio Area -->
+	<div id="portfolio" class="portfolio-area area-padding fix">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<div class="section-headline text-center">
+						<h2>Our Store</h2>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<!-- Start Portfolio -page -->
+				<div class="awesome-project-1 fix">
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+						<div class="awesome-menu ">
+							<ul class="project-menu">
+								<li>
+									<a href="#" class="active" data-filter="*">All</a>
+								</li>
+								<li>
+									<a href="#" data-filter=".development">Restaurant</a>
+								</li>
+								<li>
+									<a href="#" data-filter=".design">Supermarket</a>
+								</li>
+								<li>
+									<a href="#" data-filter=".photo">Other</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="awesome-project-content">
+					<!-- single-awesome-project start -->
+					<div class="col-md-4 col-sm-4 col-xs-12 design development">
+						<div class="single-awesome-project">
+							<div class="awesome-img">
+								<a href="#"><img src="assets/img/portfolio/1.jpg" alt="" /></a>
+								<div class="add-actions text-center">
+									<div class="project-dec">
+										<a class="venobox" data-gall="myGallery" href="img/portfolio/1.jpg">
+											<h4>Business City</h4>
+											<span>Web Development</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- single-awesome-project end -->
+					<!-- single-awesome-project start -->
+					<div class="col-md-4 col-sm-4 col-xs-12 photo">
+						<div class="single-awesome-project">
+							<div class="awesome-img">
+								<a href="#"><img src="assets/img/portfolio/2.jpg" alt="" /></a>
+								<div class="add-actions text-center">
+									<div class="project-dec">
+										<a class="venobox" data-gall="myGallery" href="assets/img/portfolio/2.jpg">
+											<h4>Blue Sea</h4>
+											<span>Photosho</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- single-awesome-project end -->
+					<!-- single-awesome-project start -->
+					<div class="col-md-4 col-sm-4 col-xs-12 design">
+						<div class="single-awesome-project">
+							<div class="awesome-img">
+								<a href="#"><img src="assets/img/portfolio/3.jpg" alt="" /></a>
+								<div class="add-actions text-center">
+									<div class="project-dec">
+										<a class="venobox" data-gall="myGallery" href="assets/img/portfolio/3.jpg">
+											<h4>Beautiful Nature</h4>
+											<span>Web Design</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- single-awesome-project end -->
+					<!-- single-awesome-project start -->
+					<div class="col-md-4 col-sm-4 col-xs-12 photo development">
+						<div class="single-awesome-project">
+							<div class="awesome-img">
+								<a href="#"><img src="assets/img/portfolio/4.jpg" alt="" /></a>
+								<div class="add-actions text-center">
+									<div class="project-dec">
+										<a class="venobox" data-gall="myGallery" href="assets/img/portfolio/4.jpg">
+											<h4>Creative Team</h4>
+											<span>Web design</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- single-awesome-project end -->
+					<!-- single-awesome-project start -->
+					<div class="col-md-4 col-sm-4 col-xs-12 development">
+						<div class="single-awesome-project">
+							<div class="awesome-img">
+								<a href="#"><img src="assets/img/portfolio/5.jpg" alt="" /></a>
+								<div class="add-actions text-center text-center">
+									<div class="project-dec">
+										<a class="venobox" data-gall="myGallery" href="assets/img/portfolio/5.jpg">
+											<h4>Beautiful Flower</h4>
+											<span>Web Development</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- single-awesome-project end -->
+					<!-- single-awesome-project start -->
+					<div class="col-md-4 col-sm-4 col-xs-12 design photo">
+						<div class="single-awesome-project">
+							<div class="awesome-img">
+								<a href="#"><img src="assets/img/portfolio/6.jpg" alt="" /></a>
+								<div class="add-actions text-center">
+									<div class="project-dec">
+										<a class="venobox" data-gall="myGallery" href="assets/img/portfolio/6.jpg">
+											<h4>Night Hill</h4>
+											<span>Photoshop</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- single-awesome-project end -->
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- awesome-portfolio end -->
+
+	<!-- JavaScript Libraries -->
+	<script src="assets/lib/jquery/jquery.min.js"></script>
+	<script src="assets/lib/bootstrap/js/bootstrap.min.js"></script>
+	<script src="assets/lib/owlcarousel/owl.carousel.min.js"></script>
+	<script src="assets/lib/venobox/venobox.min.js"></script>
+	<script src="assets/lib/knob/jquery.knob.js"></script>
+	<script src="assets/lib/wow/wow.min.js"></script>
+	<script src="assets/lib/parallax/parallax.js"></script>
+	<script src="assets/lib/easing/easing.min.js"></script>
+	<script src="assets/lib/nivo-slider/js/jquery.nivo.slider.js" type="text/javascript"></script>
+	<script src="assets/lib/appear/jquery.appear.js"></script>
+	<script src="assets/lib/isotope/isotope.pkgd.min.js"></script>
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD8HeI8o-c1NppZA-92oYlXakhDPYR7XMY"></script>
+
+	<!-- Contact Form JavaScript File -->
+	<script src="assets/contactform/contactform.js"></script>
+
+	<script src="assets/js/main.js"></script>
+	<script src="./upup.min.js"></script>
+	<script>
+		UpUp.start({
+			'content-url': 'offline.html',
+			'assets': ['./assets/img/cat-o.jpg', 'assets/css/nivo-slider-theme.css','assets/css/style.css', 'assets/css/responsive.css', 'assets/lib/bootstrap/css/bootstrap.min.css', './pawpaw.svg']
+		});
+	</script>
+</body>
+</html>
